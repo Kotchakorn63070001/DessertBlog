0ffaae8a4e3396a32ed8983c18ca2be2d69769fa
Chanisaranest <73680459+Chanisaranest@users.noreply.github.com>
2022-05-05 17:23:35 +0700

    Create IndexPage.vue

---
+++ myfrontend/src/views/IndexPage.vue
@@ -0,0 +1,24 @@
+<template>
+ 
+<div class="container" style="margin-top:10%;font-family:Times New Roman, Times, serif;">
+    <div class="columns">
+  
+  <div class="column"></div>
+  <div class="column is-one-third">
+      <article class="message">
+  <div class="message-header" style="font-size:25px">
+    <p>DessertBlog</p>
+   
+  </div>
+  <div class="message-body" style="font-size:20px">
+    <strong>Welcome to DessertBlog</strong>
+    <br>Please register and login to access the DessertBlog.
+  </div>
+</article>
+  </div>
+  <div class="column"></div>
+</div>
+
+</div>
+</template>
+
