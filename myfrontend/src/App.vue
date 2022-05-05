479bb6f6fcbba5902f84f68fb05885a26ec536f9
Chanisaranest <73680459+Chanisaranest@users.noreply.github.com>
2022-05-05 17:34:50 +0700

    Update App.vue

--- myfrontend/src/App.vue
+++ myfrontend/src/App.vue
@@ -17,7 +17,7 @@
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-end">
 
-      <a class="navbar-item" href="/home">
+      <a class="navbar-item" href="/home" v-if="user">
         <span class="icon-text">
           <span class="icon">
             <i class="fa-solid fa-house-chimney-window"></i>
@@ -26,7 +26,7 @@
         </span>
       </a>
 
-      <a class="navbar-item" >
+      <a class="navbar-item" v-if="user" >
         <router-link to="/create" class="has-text-white">
           <span class="icon-text">
             <span class="icon">
@@ -48,7 +48,7 @@
         </router-link>
       </a>
 
-      <div class="navbar-item has-dropdown is-hoverable">
+      <div class="navbar-item has-dropdown is-hoverable" v-if="user">
         <a class="navbar-link">
           <!-- <span class="icon">
             <i class="fa-solid fa-user"></i>
@@ -56,6 +56,7 @@
           <figure class="image is-24x24 my-auto">
             <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
           </figure>
+          <span class="pl-3">{{ user.username }}</span>
         </a>
 
         <div class="navbar-dropdown is-right">
@@ -65,14 +66,14 @@
             </router-link>
           </a>
           <hr class="navbar-divider">
-          <a class="navbar-item">
+          <a class="navbar-item" @click="logout()">
             Log out
           </a>
         </div>
       </div>
 
     
-        <div class="navbar-item" >
+        <div class="navbar-item"  v-if="!user">
           <div class="buttons">
             <a class="button is-primary">
               <router-link to="#" class="has-text-white">
@@ -85,9 +86,44 @@
     </div>
   </div>
 </nav>
-    <router-view :key="$route.fullPath" />
+   <router-view :key="$route.fullPath" @auth-change="onAuthChange" :user="user" />
   </div>
 </template>
+<script>
+import axios from '@/plugins/axios'
+
+export default {
+  data () {
+    return {
+      user: null
+    }
+  },
+  mounted () {
+    this.onAuthChange()
+  },
+  methods: {
+      logout () {
+        localStorage.clear()
+        this.user = null
+        // this.$router.push('/user/login')
+        this.$router.replace({ name: "login"}).catch(()=>{});
+      },
+    onAuthChange () {
+      const token = localStorage.getItem('token')
+      if (token) {
+        this.getUser()
+      }
+    },
+    getUser () {
+      const token = localStorage.getItem('token')
+      console.log(token)
+      axios.get('/user/me').then(res => {
+        this.user = res.data
+      })
+    },
+  },
+}
+</script>
 
 
 
